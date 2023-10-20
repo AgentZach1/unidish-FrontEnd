e0fde780bd7a6c1358d4633be24e1ec2059c6fef
AgentZach1 <owlmanzach10@gmail.com>
2023-10-20 13:37:07 -0400

    changed unidish server doc

--- db_unidish_server.js
+++ db_unidish_server.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
 
+// Imagine this is in another folder and also as an amorphous process (service)
+
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 const https = require('https');
 const fs = require('fs');
-const port = process.env.PORT || 3305;
 
 const app = express();
+const port = 5000;
+const limit = 1000;
+
+const tableName = "readings_tent";
 
 const corsOptions = {
     origin: ['https://connect.weiss.land', 'http://localhost:3000'],
@@ -25,26 +30,72 @@ const db = mysql.createConnection({
     database: process.env.DB_DATABASE
 });
 
+// New MySQL Connection for Unidish
+const unidishDb = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.UNIDISH_DB_USER,
+    password: process.env.UNIDISH_DB_PASSWORD,
+    database: process.env.UNIDISH_DB_DATABASE
+});
+
 db.connect((err) => {
     if (err) throw err;
     console.log('Connected to the database');
-    console.log('User: ' + process.env.DB_USER);
   });
+  
+// Connect to Unidish database
+unidishDb.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to the Unidish database ooooooo');
+});
 
-// Define the endpoint
 app.get('/api/unidish', (req, res) => {
-    console.log("Received get");
+    console.log('Received request for /api/unidish');
     const { table } = req.query;
     let query = '';
-    query = 'SELECT * FROM ' + table + ';';
 
-    db.query(query, (err, results) => {
+    if (table == 'all') {
+        query = 'SELECT * FROM COMMENT, COMMENT_DISLIKES, COMMENT_LIKES, DINING_HALL, MENU_ITEM, RESTAURANT, REVIEW, REVIEW_DISLIKES, REVIEW_LIKES, USER;'
+    }
+    else {
+        query = 'SELECT * FROM ' + table + ';';
+    }
+
+    unidishDb.query(query, (err, results) => {
         if (err) throw err;
         res.json(results);
     })
 });
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+// Define the endpoint
+app.get('/api/data', (req, res) => {
+    const { topic } = req.query;
+    let query = '';
+
+    // Assign the query based on the mqttTopic
+    if (topic == 'co2') {
+        query = 'SELECT * FROM (SELECT id, date, carbon_dioxide AS value FROM ' + tableName + ' order by id desc LIMIT ' + limit + ') AS subquery ORDER BY subquery.id ASC';
+    } else if (topic == 'temp') {
+        query = 'SELECT * FROM (SELECT id, date, temperature AS value FROM ' + tableName + ' order by id desc LIMIT ' + limit + ') AS subquery ORDER BY subquery.id ASC';
+    } else if (topic == 'hum') {
+        query = 'SELECT * FROM (SELECT id, date, humidity AS value FROM ' + tableName + ' order by id desc LIMIT ' + limit + ') AS subquery ORDER BY subquery.id ASC';
+    } else if (topic == 'light') {
+        query = 'SELECT * FROM (SELECT id, date, light AS value FROM ' + tableName + ' order by id desc LIMIT ' + limit + ') AS subquery ORDER BY subquery.id ASC';
+    } else {
+        res.status(400).json({ error: 'Invalid topic' });
+        return;
+    }
+    console.log("Executing query for " + topic + " and returning")
+    // Execute the query and return the results
+    db.query(query, (err, results) => {
+        if (err) throw err;
+        res.json(results);
     });
-    
+});
+
+
+
+
+app.listen(port, () => {
+console.log(`Server running on port ${port}`);
+});
